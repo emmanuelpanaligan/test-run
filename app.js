67e6435c174aae56d8bb0062ba1fd9b25055b1aa
Emmanuel Panaligan <panalige@gmail.com>
2018-05-18 14:06:14 -0400

    updates

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.get("/results", function(req, res){
 });
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT;
 
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
 
 app.listen(server_port, server_ip_address, function () {
 
